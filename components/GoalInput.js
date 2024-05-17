d39fce27e19cd97207e12362de23061ab67164d8
iAaquibjawed <mallickaaquib@gmail.com>
2024-05-17 21:46:33 +0530

    added images

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import {View, TextInput, Button, StyleSheet, Modal} from 'react-native';
+import {View, TextInput, Button, StyleSheet, Modal, Image} from 'react-native';
 import { useState } from'react';
 
 
@@ -20,6 +20,7 @@ function GoalInput(props) {
     return (
         <Modal visible={props.visible} animationType="slide">
           <View style={styles.inputContainer}>
+            <Image style={styles.image} source={require('../assets/images/goal.png')}/>
               <TextInput placeholder='Your course goal!' style={styles.textInput} onChangeText={goalInputHandler}
             value={enteredGoalText}
             />
@@ -45,10 +46,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent:'center',
         alignItems: 'center',
-        marginBottom: 24,
         padding: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
+        backgroundColor: '#311b6b'
     
       },
       textInput:{
@@ -65,5 +64,10 @@ const styles = StyleSheet.create({
       button:{
         width: '40%',
         marginHorizontal: 8
+      },
+      image:{
+        width: 100,
+        height: 100,
+        margin: 20
       }
 });
